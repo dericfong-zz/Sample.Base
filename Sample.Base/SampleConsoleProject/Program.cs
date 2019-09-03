dea0b4ea79d765fb927827966001187ab7777892
fong <fong9931@hotmail.com>
2019-09-03 10:07:13 +0800

    add sample console to test artifacts package

---
+++ Sample.Base/SampleConsoleProject/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Sample.Base.Core;
+
+
+namespace SampleConsoleProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Test Azure Artifacts implementation \"Sample.Base.Core\"");
+            string testValue = string.Empty;
+
+            testValue = " ";
+            testValue = testValue.TrimToEmpty();
+            Console.WriteLine($"TrimToEmpty-{testValue}-");
+
+            testValue = " ";
+            testValue = testValue.TrimToNull();
+            Console.WriteLine($"TrimToNull-{testValue == null}-");
+
+            testValue = " ";
+            var boolV = testValue.IsNullOrEmpty();
+            Console.WriteLine($"-{boolV}-");
+
+            Console.WriteLine("End Test");
+        }
+    }
+    
+}
