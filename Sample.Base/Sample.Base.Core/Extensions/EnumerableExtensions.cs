9ac1bf06e3aa594047389449d54186cc393ebc16
fong <fong9931@hotmail.com>
2019-09-18 13:17:54 +0800

    update

--- Sample.Base/Sample.Base.Core/Extensions/EnumerableExtensions.cs
+++ Sample.Base/Sample.Base.Core/Extensions/EnumerableExtensions.cs
@@ -20,5 +20,27 @@ namespace Sample.Base.Core.Extensions
             }
             return !enumerable.Any();
         }
+
+        public static IEnumerable<int> IndexesWhere<T>(this IEnumerable<T> source, Func<T, bool> predicate)
+        {
+            int index = 0;
+            foreach (T item in source)
+            {
+                if (predicate(item))
+                {
+                    yield return index;
+                }
+                index++;
+            }
+        }
+
+        public static T ElementAtOrDefault<T>(this IEnumerable<T> list, int index, T @default)
+        {
+            if (index < 0 || index >= list.Count())
+            {
+                return @default;
+            }
+            return list.ElementAt(index);
+        }
     }
 }
