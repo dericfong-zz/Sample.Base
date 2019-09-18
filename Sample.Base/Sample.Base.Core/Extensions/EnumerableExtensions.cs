248fe053a9625c567948049c36b347bbf4ebfafe
fong <fong9931@hotmail.com>
2019-09-18 11:53:31 +0800

    add enumerable class

---
+++ Sample.Base/Sample.Base.Core/Extensions/EnumerableExtensions.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Sample.Base.Core.Extensions
+{
+    public static class EnumerableExtensions
+    {
+        public static bool IsNullOrEmpty<T>(this IEnumerable<T> enumerable)
+        {
+            if (enumerable == null)
+            {
+                return true;
+            }
+            ICollection<T> collection;
+            if ((collection = (enumerable as ICollection<T>)) != null)
+            {
+                return collection.Count < 1;
+            }
+            return !enumerable.Any();
+        }
+    }
+}
