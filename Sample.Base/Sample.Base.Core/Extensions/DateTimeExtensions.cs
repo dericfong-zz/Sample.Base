6e85fc93e65b1ff332ce2ec1cd550cbe57aad426
fong <fong9931@hotmail.com>
2019-12-27 16:18:50 +0800

    add datetime extensions

---
+++ Sample.Base/Sample.Base.Core/Extensions/DateTimeExtensions.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Sample.Base.Core.Extensions
+{
+    public static class DateTimeExtensions
+    {
+        public static string ToFormat12h(this DateTime? dt)
+        {
+            return dt == null ? "" : Convert.ToDateTime(dt).ToString("yyyy/MM/dd hh:mm:ss tt");
+        }
+    }
+}
